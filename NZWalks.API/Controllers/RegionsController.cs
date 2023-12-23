47ada9d536ff3283bf086015cba78a6a00059ec7
Daniela Vescia <danivescia@gmail.com>
2023-12-23 18:09:03 -0300

    added  dto

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route( "api/[controller]" )]
+    [ApiController]
+
+    public class RegionsController : ControllerBase
+    {
+
+        private readonly NzWalksDbContext dbContext;
+
+        //constructor
+        public RegionsController( NzWalksDbContext dbContext )
+        {
+            this.dbContext = dbContext;
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regionsDomain = dbContext.Regions.ToList();
+
+            var regionsDto = new List<RegionDto>();
+            foreach ( var regionDomain in regionsDomain ) 
+            {
+                regionsDto.Add( new RegionDto()
+                {
+                    Id = regionDomain.Id,
+                    Code = regionDomain.Code,
+                    Name = regionDomain.Name,
+                    RegionImageUrl = regionDomain.RegionImageUrl,
+
+                });
+            }
+
+            return Ok( regionsDto );
+
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetRegionById([FromRoute] Guid id )
+        {
+            var regionDomain = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if ( regionDomain == null ) 
+            { 
+                return NotFound();
+            }
+
+            //Map Region Domain Model to Region DTO
+
+            var regionDto = new RegionDto
+            {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl,
+
+            };
+
+            return Ok( regionDto );
+        }
+    }
+}
